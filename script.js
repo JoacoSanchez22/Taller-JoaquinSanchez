004af5b83415ef4367b31f38975de18877594673
JoacoSanchez22 <nasiff02@gmail.com>
2026-02-06 15:49:20 -0300

    Le cambie imagenes

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const buyBtn = document.getElementById("buyBtn");
+// Seleccionamos todos los botones de las tarjetas
+const buyButtons = document.querySelectorAll(".buyBtn");
 const form = document.getElementById("contactForm");
 
-buyBtn.addEventListener("click", () => {
-  alert("Producto agregado al carrito 🛒");
+// Función para los botones de compra
+buyButtons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    const modelo = e.target.getAttribute("data-name");
+    alert(`¡${modelo} agregado al carrito! 🛒`);
+  });
 });
 
+// Función para el formulario de contacto
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Gracias por contactarnos 👟");
